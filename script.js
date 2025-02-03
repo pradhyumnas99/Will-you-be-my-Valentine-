a398d2acace9beb2f1bb6446e61e7240dcb9f29e
pradhyumnas99 <145165572+pradhyumnas99@users.noreply.github.com>
2025-02-03 14:41:56 -0500

    updates

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Ekdum Pakka??",
     "Just think about it!",
     "If you say no, I will be really sad...",
-    "I will be very sad...",
+    "Meri feelings ka socho...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
